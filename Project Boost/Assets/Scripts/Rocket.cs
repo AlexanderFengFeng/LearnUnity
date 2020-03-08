73630063b08b9252f06f8968ec53c4dad9790894
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2020-03-08 06:32:39 -0700

    Add rocket controls.

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        print("yo");
     }
 
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            
+            print("yo2");
+            rb.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
         }
     }
 }
